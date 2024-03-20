f64ee03ed344a521c31feb9a880d00cf62333f9d
LC Team <lilcoomers.esi@gmail.com>
2024-03-20 14:16:06 +0330

    edit

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import pygame
+from Close_button import Close_button
 
 pygame.init()
 
@@ -8,12 +9,13 @@ pygame.init()
 
 
 "Screens"
-window = pygame.display.set_mode((1800, 1000), pygame.NOFRAME)
-main_background = pygame.Surface((1600, 800))
-bar_background = pygame.Surface((1800, 50))
+window = pygame.display.set_mode((1600, 1000), pygame.NOFRAME)
+main_background = pygame.Surface((1600, 950))
+bar_background = pygame.Surface((1600, 50))
 
 "images"
 image = pygame.image.load('F:/Mohammad/Anime-images/desktop-wallpaper-cute-scary-anime-horror-anime-pfp.jpg')
+font = pygame.font.SysFont('Arial', 20)
 
 "Loop"
 run_Key = True
@@ -21,10 +23,10 @@ while run_Key:
 
     "default color"
     window.fill((155, 155, 155))
-    bar_background.fill((90, 255, 45))
+    bar_background.fill((200, 200, 200))
 
     "background"
-    window.blit(main_background, (100, 50))
+    window.blit(main_background, (0, 50))
     window.blit(bar_background, (0, 0))
 
     "Events"
@@ -32,11 +34,15 @@ while run_Key:
         if event.type == pygame.QUIT:
             run_Key = False
 
-    pygame.draw.circle(main_background, (0, 0, 255), (800, 400), 75)
+    pygame.draw.circle(main_background, (0, 0, 255), (800, 475), 75)
+
+    "Close button"
+    # Close_button(window, font, 'close').process()
 
     "refresh page"
     pygame.display.flip()
     # pygame.display.update()
+    pygame.time.Clock().tick(60)
 
 "End"
 pygame.quit()
