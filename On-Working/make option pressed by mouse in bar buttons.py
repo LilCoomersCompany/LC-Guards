27ddff2ca8f983bfb07bd102f3ac75ba58dacd26
LC Team <lilcoomers.esi@gmail.com>
2024-03-22 14:37:53 +0330

    update

--- On-Working/make option pressed by mouse in bar buttons.py
+++ On-Working/make option pressed by mouse in bar buttons.py
@@ -1,33 +1,111 @@
+import time
+
 import pygame
 
-# Initialize Pygame
+"run requirements"
+import sys
+
 pygame.init()
 
-# Create a Pygame window
-screen = pygame.display.set_mode((800, 600))
+width, height = 1800, 1000
+screen = pygame.display.set_mode((width, height), pygame.NOFRAME)
+main_background = pygame.Surface((1800, 950))
+bar_background = pygame.Surface((1800, 50))
 
-# Previous state of the left mouse button
-previous_left_mouse_button_state = False
+font = pygame.font.SysFont('Arial', 10)
 
-running = True
-while running:
+previous_mouse = False
+"run requirements"
+while True:
+    screen.fill((140, 45, 40))
+    bar_background.fill((90, 255, 45))
+    screen.blit(bar_background, (0, 0))
+    main_background.fill((140, 80, 41))
+    screen.blit(main_background, (0, 50))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
+            sys.exit()
+    width = 40
+    height = 40
+    buttons = [
+        {
+            'name': "Close",
+            'location': 1560
+        },
+        {
+            'name': "FullScreen",
+            'location': 1520
+        },
+        {
+            'name': "Minimize",
+            'location': 1480
+        }
+    ]
+    buttonRects = []
+    buttonSurfs = []
+    buttonSurfaces = []
 
-    # Get the current state of the left mouse button
-    current_left_mouse_button_state = pygame.mouse.get_pressed()[0]
+    current_mouse = pygame.mouse.get_pressed()[0]
 
-    # Check if the left mouse button was previously pressed and is now released
-    if previous_left_mouse_button_state and not current_left_mouse_button_state:
-        print("Left mouse button released")
+    fillColors = {
+        'normal': (64, 64, 64),
+        'hover': (80, 80, 80),
+        'pressed': (115, 115, 115),
+        'special_hover': (255, 0, 10),
+        'special_pressed': (157, 0, 6)
+    }
 
-        # Place your code here to execute when the left mouse button is released
+    for button in buttons:
+        buttonSurface = pygame.Surface((width, height))
+        buttonRect = pygame.Rect(button['location'], 0, width, height)
+        buttonSurf = font.render(button['name'], True, (255, 255, 255))
+        buttonSurface.fill(fillColors['normal'])
+        buttonRects.append(buttonRect)
+        buttonSurfs.append(buttonSurf)
+        buttonSurfaces.append(buttonSurface)
 
-    # Update the previous state of the left mouse button
-    previous_left_mouse_button_state = current_left_mouse_button_state
+    for button in buttons:
+        buttonSurfaces[buttons.index(button)].blit(buttonSurfs[buttons.index(button)], [
+            buttonRects[buttons.index(button)].width / 2 - buttonSurfs[
+                buttons.index(button)].get_rect().width / 2,
+            buttonRects[buttons.index(button)].height / 2 - buttonSurfs[
+                buttons.index(button)].get_rect().height / 2
+        ])
+        screen.blit(buttonSurfaces[buttons.index(button)],
+                    buttonRects[buttons.index(button)])
+
+    mousePos = pygame.mouse.get_pos()
+    for button in buttons:
+        if buttonRects[buttons.index(button)].collidepoint(mousePos):
+            if button['name'] == "Close":
+                buttonSurfaces[buttons.index(button)].fill(fillColors['special_hover'])
+                if previous_mouse:
+                    buttonSurfaces[buttons.index(button)].fill(fillColors['special_pressed'])
+                    if not current_mouse:
+                        pygame.quit()
+            elif button['name'] == "Minimize" or button['name'] == "FullScreen":
+                buttonSurfaces[buttons.index(button)].fill(fillColors['hover'])
+                if previous_mouse:
+                    if button['name'] == "Minimize":
+                        buttonSurfaces[buttons.index(button)].fill(fillColors['pressed'])
+                        if not current_mouse:
+                            pygame.display.iconify()
+                    elif button['name'] == "FullScreen":
+                        buttonSurfaces[buttons.index(button)].fill(fillColors['pressed'])
+                        if not current_mouse:
+                            pygame.display.toggle_fullscreen()
+
+            buttonSurfaces[buttons.index(button)].blit(buttonSurfs[buttons.index(button)], [
+                buttonRects[buttons.index(button)].width / 2 - buttonSurfs[
+                    buttons.index(button)].get_rect().width / 2,
+                buttonRects[buttons.index(button)].height / 2 - buttonSurfs[
+                    buttons.index(button)].get_rect().height / 2
+            ])
+            screen.blit(buttonSurfaces[buttons.index(button)],
+                        buttonRects[buttons.index(button)])
+
+    previous_mouse = current_mouse
 
     pygame.display.flip()
-
-# Quit Pygame
-pygame.quit()
